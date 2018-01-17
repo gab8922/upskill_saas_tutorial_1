5a2b225d0a923ee84f0c0e495ce10982d48c06c3
Gabriela Salazar <gabriela.s1989@gmail.com>
2018-01-17 01:28:48 +0000

    Added Stripe JS library and pro form id

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,13 @@
+/* global $ */
+//Document ready.
+$(document).on('turbolinks:load', function(){
+//Set Stripe public key.
+//When user clicks form submit btn.
+//prevent default submission behavior.
+
+//Collect the credit card fields.
+//Send the card info to Stripe.
+//Stripe will return a card token.
+//Inject card token as hidden field into form.
+//Submit form to our Rails app.
+});
